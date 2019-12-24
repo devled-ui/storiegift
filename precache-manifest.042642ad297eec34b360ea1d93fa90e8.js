115c4993b86594f23706e3387a79be9ac3e51efa
devled-ui <vova.ledok@gmail.com>
2019-12-24 15:38:19 +0500

    Updates

--- precache-manifest.042642ad297eec34b360ea1d93fa90e8.js
+++ precache-manifest.042642ad297eec34b360ea1d93fa90e8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6065ba867ab86f9f4c6510b1287822bc",
+    "revision": "3370d694e599a598bf4c0038bbe3e55e",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "eaaee22431e5b0c61a15",
+    "revision": "756b2848873be09384ec",
     "url": "./static/css/main.a0b03332.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8bb62c5d.chunk.js.LICENSE"
   },
   {
-    "revision": "eaaee22431e5b0c61a15",
-    "url": "./static/js/main.030bb860.chunk.js"
+    "revision": "756b2848873be09384ec",
+    "url": "./static/js/main.7708d830.chunk.js"
   },
   {
     "revision": "adcadff82edf95cf1bc7",
